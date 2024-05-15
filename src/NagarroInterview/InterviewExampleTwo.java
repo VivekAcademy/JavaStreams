c7a4641cd648eba7232d64bc5f12f53ebbd76dd7
Vivek Jain <vivekj229@gmail.com>
2024-05-15 19:25:35 +0530

    New Code Committed From Mac

--- src/NagarroInterview/InterviewExampleTwo.java
+++ src/NagarroInterview/InterviewExampleTwo.java
@@ -1,5 +1,7 @@
 package NagarroInterview;
 
+import java.util.stream.Stream;
+
 public class InterviewExampleTwo {
 
 	public InterviewExampleTwo() {
@@ -7,6 +9,11 @@ public class InterviewExampleTwo {
 	}
 
 	public static void main(String[] args) {
+
+		Stream.iterate(new int[] {0,1}, f -> new int[] {f[1] , f[1]+f[0]})
+				.limit(10)
+				.map(f->f[0])
+				.forEach(System.out::println);
 		
 	}
 
